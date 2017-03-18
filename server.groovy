ae9ee0d878680c87c91ab774d61a8b4a23e921c9
Keith Suderman <suderman@cs.vassar.edu>
2017-03-18 15:51:00 -0400

    Added service to assign PID values to services based on their URL

--- server.groovy
+++ server.groovy
@@ -1,11 +1,11 @@
+@Grab(group='org.mortbay.jetty', module='jetty-embedded', version='6.1.26')
 import org.mortbay.jetty.Server
 import org.mortbay.jetty.servlet.*
 import groovy.servlet.*
  
-@Grab(group='org.mortbay.jetty', module='jetty-embedded', version='6.1.26')
 def startJetty() {
     def jetty = new Server(8888)
-     
+
     def context = new Context(jetty, '/', Context.SESSIONS)  // Allow sessions.
     context.resourceBase = '.'  // Look in current dir for Groovy scripts.
     context.addServlet(GroovyServlet, '/*')  // All files ending with .groovy will be served.
