e63004f6370c4744291b2a9a4561f8cfea481ee2
Keith Suderman <suderman@cs.vassar.edu>
2017-06-20 15:00:03 -0400

    Accepts plain text as input

--- src/test/bash/proxy-test.sh
+++ src/test/bash/proxy-test.sh
@@ -2,41 +2,53 @@
 
 file="src/test/resources/karen.lif"
 
-if (ps aux | grep server.groovy | grep java) ; then
-	echo "Groovy server is already running"
-else
-	echo "Please start the server.groovy script first."
-	exit 1
-fi
-
 type="Content-type: application/json"
 url="LAPPS-PROXY-URL: http://vassar.lappsgrid.org/invoker/anc:stanford.tokenizer_2.0.0"
 username="LAPPS-PROXY-USERNAME: weblicht" 
 password="LAPPS-PROXY-PASSWORD: weblicht"
 
+server=http://localhost:8888/soap-proxy
+if [ "$1" = "remote" ] ; then
+    server=http://api.lappsgrid.org/soap-proxy
+    shift
+else
+    # Ensure the local server is running.
+    if (ps aux | grep server.groovy | grep java) ; then
+        echo "Using the local Groovy server."
+    else
+        echo "Please start the server.groovy script first."
+        exit 1
+    fi
+fi
+
 if [ -z "$1" ] ; then
-    curl -i -X POST -H "$type" -H "$url" -d @src/test/resources/karen.lif http://localhost:8888/soap-proxy
+    echo "URL via headers to $server"
+    curl -i -X POST -H "$type" -H "$url" -d @src/test/resources/karen.lif $server
     exit 0
 fi
 
+echo "Testing $1 on $server"
 case $1 in
     headers)
-        curl -i -X POST -H "$type" -H "$url" -H "$username" -H "$password" --data-binary @src/test/resources/karen.lif http://localhost:8888/soap-proxy
+        curl -i -X POST -H "$type" -H "$url" -H "$username" -H "$password" --data-binary @src/test/resources/karen.lif $server
         ;;
     url)
-        curl -i -X POST -H "$type" -H "$url" -d @src/test/resources/karen.lif http://localhost:8888/soap-proxy
+        curl -i -X POST -H "$type" -H "$url" -d @src/test/resources/karen.lif $server
         ;;
     params)
-        curl -i -X POST -H "$type" -d @src/test/resources/karen-parameters.lif http://localhost:8888/soap-proxy
+        curl -i -X POST -H "$type" -d @src/test/resources/karen-parameters.lif $server
         ;;
     baduser)
-        curl -i -X POST -H "$type" -H "$url" -H "LAPPS-PROXY-USERNAME: unknown" -H "$password" --data-binary @src/test/resources/karen.lif http://localhost:8888/soap-proxy
+        curl -i -X POST -H "$type" -H "$url" -H "LAPPS-PROXY-USERNAME: unknown" -H "$password" --data-binary @src/test/resources/karen.lif $server
         ;;
     badpass)
-        curl -i -X POST -H "$type" -H "$url" -H "$username" -H "LAPPS-PROXY-PASSWORD: n0s3cr37" --data-binary @src/test/resources/karen.lif http://localhost:8888/soap-proxy
+        curl -i -X POST -H "$type" -H "$url" -H "$username" -H "LAPPS-PROXY-PASSWORD: n0s3cr37" --data-binary @src/test/resources/karen.lif $server
+        ;;
+    text)
+        curl -i -X POST -H "Content-type: text/plain" -H "$url" -d "Karen flew to New York." $server
         ;;
     *)
-        echo "Invalid option: one of headers, url, params, baduser, badpass"
+        echo "Invalid option: one of headers, url, params, baduser, badpass, or text"
         exit 1
         ;;
 esac
