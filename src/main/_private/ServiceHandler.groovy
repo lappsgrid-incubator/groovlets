7405d60d6d97e16d390e3e05d460339d24bfc272
Keith Suderman <suderman@cs.vassar.edu>
2017-08-05 00:01:10 -0500

    Before cleanup

--- src/main/_private/ServiceHandler.groovy
+++ src/main/_private/ServiceHandler.groovy
@@ -1,5 +1,6 @@
 package src.main._private
 
+//import groovy.util.logging.Slf4j
 @GrabConfig(systemClassLoader=true)
 @Grab("net.servicegrid:jp.go.nict.langrid.client:1.0.5")
 import jp.go.nict.langrid.client.RequestAttributes
@@ -31,6 +32,7 @@ import javax.servlet.http.HttpServletResponse
 //    return
 //}
 
+//@Slf4j("logger")
 class ServiceHandler {
 
     String url
@@ -44,22 +46,36 @@ class ServiceHandler {
     def response
     def html
 
+    Map searchTerms
+
     public ServiceHandler(def parent) {
+//        logger.info("Initializing handler")
         params = parent.params
         headers = parent.headers
         out = parent.out
         response = parent.response
         html = parent.html
+        searchTerms = [
+                id:'serviceId',
+                name: 'serviceName',
+                desc: 'serviceDescription',
+                type: 'serviceType',
+                domain: 'serviceTypeDomain'
+        ]
     }
 
     void handle() { //def params, def headers, def out, def response, def html) {
+
+//        logger.debug("processing parameters")
         def conditions = new MatchingCondition[params.size()]
         int i = 0
         params.each { name,value ->
+            name = searchTerms[name] ?: name
             conditions[i] = new MatchingCondition(name, value, "PARTIAL")
             ++i
         }
 
+//        logger.debug("creating soap client")
         def order = [] as Order[]
         SoapClientFactory f = new SoapClientFactory();
         ServiceManagementService s = f.create(
@@ -79,6 +95,7 @@ class ServiceHandler {
         // The ServiceManager only allows us to fetch metadata for 100 services at a
         // time. So we have to be prepared to page through the entire list if more than
         // 100.
+//        logger.debug("paging data")
         int PAGE_SIZE = 100
         int count = 0
         ServiceEntrySearchResult more = s.searchServices(count, PAGE_SIZE, conditions, order, "ALL");
@@ -89,11 +106,13 @@ class ServiceHandler {
         }
         result.totalCount = count
         if (headers.Accept == '*/*' || headers.Accept?.contains('application/json')) {
+//            logger.debug("returning application/json")
             response.status = 200
             response.contentType = 'application/json'
             out.println new groovy.json.JsonBuilder(result).toPrettyString()
         }
         else if (headers.Accept?.contains('text/html')) {
+//            logger.debug("returning text/html")
             File template = new File('src/main/templates/services.gsp')
             result.node = node
 //            result.heading = heading //"LAPPS Services on the Vassar node"
@@ -105,6 +124,7 @@ class ServiceHandler {
             script.run()
         }
         else {
+//            logger.warn("Invalid accept header: {}", headers.Accept)
             response.status = 406
             response.addHeader('Accept', 'application/json, text/html')
             out.println "Please request application/json or text/html"
